ffefe9fdc2c61ad47067476c8d879d36aef1ddc8
lyoko90210 <danieldennis801@gmail.com>
2024-07-25 19:46:31 -0400

    update Readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Management-system
 
+## overview 
+This command-line application is designed to manage a company's employee database 
+
+## Technologies Used
+- Node.js
+- PostgreSQL
+- npm
+- Inquirer.js
+
+
+## watch it run 
+https://youtu.be/KVqLcpi3Vn8?si=kokMxSQHtyNcKhwN
+
+## Feedback and Contributions
+I'm continuously working to improve the my projects If you have any feedback, suggestions, or would like to contribute to the project, please feel free to Contact me ( lyoko1629@gmail.com or @ https://linktr.ee/Lyoko90210 )
--- index.js
+++ index.js
@@ -169,6 +169,25 @@ function updateEmployeeRole() {
     });
 }
 
-
+ function updateEmployeeRole () {
+    prompt([
+        {
+            type: 'input',
+            name: 'employee_id',
+            message: 'Enter employee ID:'
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'Enter new role ID:'
+        }
+    ]).then((answer) => {
+        pool.query('UPDATE Employee SET role_id = $1 WHERE id = $2', [answer.role_id, answer.employee_id], (err, res) => {
+            if (err) throw err;
+            console.log('Employee role updated!');
+            start();
+        });
+    });
+}
 
 start()
